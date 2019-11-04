6f48c587f4a14b4563bfd71a9d2cde3844ed246b
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-04 09:45:41 -0600

    add database

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,20 +1,20 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 
-// const url = require('url');
-// if (process.env.DATABASE_URL) {
-//     const params = url.parse(process.env.DATABASE_URL);
-//     const auth = params.auth.split(':');
-// }
+const url = require('url');
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+}
 
 const pool = new Pool({
-//     database: params.pathname.split('/')[1],
-//     host: params.hostname,
-//     port: params.port,
-//     max: 10,
-//     idleTimeoutMillis: 30000,
-//     ssl: true
-// } || {
+    database: params.pathname.split('/')[1],
+    host: params.hostname,
+    port: params.port,
+    max: 10,
+    idleTimeoutMillis: 30000,
+    ssl: true
+} || {
     database: process.env.DATABASE_NAME || 'react-calculator',
     host: 'localhost',
     port: 5432,
@@ -27,7 +27,7 @@ pool.on('connect', () => {
 })
 pool.on('error', () => {
     console.log('error connecting pool to database');
-    // process.exit(-1);
+    process.exit(-1);
 })
 
 module.exports = pool;
