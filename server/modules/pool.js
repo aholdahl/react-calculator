5a8ba970d4309a78cde0236afab1380800802fdd
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-04 09:39:37 -0600

    updated database to detect heroku database

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,8 +1,21 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 
+// const url = require('url');
+// if (process.env.DATABASE_URL) {
+//     const params = url.parse(process.env.DATABASE_URL);
+//     const auth = params.auth.split(':');
+// }
+
 const pool = new Pool({
-    database: 'react-calculator',
+//     database: params.pathname.split('/')[1],
+//     host: params.hostname,
+//     port: params.port,
+//     max: 10,
+//     idleTimeoutMillis: 30000,
+//     ssl: true
+// } || {
+    database: process.env.DATABASE_NAME || 'react-calculator',
     host: 'localhost',
     port: 5432,
     max: 10,
@@ -14,6 +27,7 @@ pool.on('connect', () => {
 })
 pool.on('error', () => {
     console.log('error connecting pool to database');
+    // process.exit(-1);
 })
 
 module.exports = pool;
