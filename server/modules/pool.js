1810fc4e29f2a0ca18da843e8e36c872c0ae8c34
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-04 09:57:09 -0600

    fixed pool issue

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,33 +1,41 @@
 const pg = require('pg');
-const Pool = pg.Pool;
-
 const url = require('url');
+
+let config = {};
+
 if (process.env.DATABASE_URL) {
     const params = url.parse(process.env.DATABASE_URL);
     const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+} else {
+    config = {
+        host: 'localhost',
+        port: 5432,
+        database: 'react-calculator',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
 }
 
-const pool = new Pool({
-    database: params.pathname.split('/')[1],
-    host: params.hostname,
-    port: params.port,
-    max: 10,
-    idleTimeoutMillis: 30000,
-    ssl: true
-} || {
-    database: process.env.DATABASE_NAME || 'react-calculator',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-})
+const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
-    console.log('pool connected to database');
-})
-pool.on('error', () => {
-    console.log('error connecting pool to database');
+    console.log('Postgesql connected');
+});
+
+pool.on('error', (err) => {
+    console.log('Unexpected error on idle client', err);
     process.exit(-1);
-})
+});
 
 module.exports = pool;
